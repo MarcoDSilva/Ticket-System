76081d57e8aea8e9353e244f0db57a0cf8db3fb6
MarcoDSilva <38049196+MarcoDSilva@users.noreply.github.com>
2020-01-27 17:13:53 +0000

    starting the Origem controller/db class

---
+++ TicketSystem/Controllers/OrigensController.vb
@@ -0,0 +1,41 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class OrigensController
+        Inherits Controller
+
+        'conexao com a bd/tabela origem
+        Private conectaBD As New Manipula_TOrigem
+
+        ' GET: Origens
+        Function Index() As ActionResult
+            Return View(LeituraDados("SELECT * FROM Origem;"))
+        End Function
+
+        ''' <summary>
+        ''' Método interno utilizado para ler dados da bd
+        ''' </summary>
+        ''' <param name="query"></param>
+        ''' <returns></returns>
+        Function LeituraDados(query As String) As List(Of Origem)
+
+            Dim tabelaDados As DataTable = conectaBD.LeituraTabela(query)
+            Dim listagemOrigens As List(Of Origem) = New List(Of Origem)
+
+            'fazemos um ciclo, que vai iterar por cada elemento que recebenos da base de dados
+            'criamos um novo objecto do tipo Prioridades, onde atribuimos os dados da iteração actual
+            'e no fim após a atribuição desses dados, inserimos numa List(a) de Prioridades, o qual usamos para retornar os dados
+            For Each item In tabelaDados.AsEnumerable
+                Dim org As New Origem()
+
+                org.ID_origem = item(0)
+                org.descricao = item(1)
+                org.dat_hor = item(2)
+
+                listagemOrigens.Add(org)
+            Next
+
+            Return listagemOrigens
+        End Function
+    End Class
+End Namespace
---
+++ TicketSystem/Utils/Manipula_TOrigem.vb
@@ -0,0 +1,44 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Manipula_TOrigem
+    Inherits ObterDados
+
+    'conexao da SC
+    Private ReadOnly conexao As New SqlConnection(Conector.stringConnection)
+
+    Public Sub AdicionarOrigem(descricao As String)
+        Dim query As String = "INSERT INTO Origem VALUES (@org, CURRENT_TIMESTAMP);"
+        Dim comando As New SqlCommand(query, conexao)
+
+        comando.Parameters.AddWithValue("@org", descricao)
+        ExecutaComandos(comando)
+    End Sub
+
+    Public Sub EditarOrigem(descricao As String, ID_origem As Integer)
+        Dim query As String = $"UPDATE Origem SET origem = @org , dat_hor = CURRENT_TIMESTAMP WHERE ID_origem = {ID_origem};"
+        Dim comando As New SqlCommand(query, conexao)
+
+        comando.Parameters.AddWithValue("@org", descricao)
+        ExecutaComandos(comando)
+    End Sub
+
+    Public Sub ApagarOrigem(ID_origem As Integer)
+        Dim query As String = $"DELETE FROM Origem WHERE ID_origem = {ID_origem};"
+        Dim comando As New SqlCommand(query, conexao)
+
+        ExecutaComandos(comando)
+    End Sub
+
+    ''' <summary>
+    ''' Método que executa os comandos/queries a serem executadas na base de dados
+    ''' Utilizado apenas para evitar repetição no código
+    ''' </summary>
+    ''' <param name="comando"></param>
+    Public Sub ExecutaComandos(comando As SqlCommand)
+        conexao.Open()
+        comando.ExecuteNonQuery()
+        comando.Parameters.Clear()
+        conexao.Close()
+    End Sub
+
+End Class
